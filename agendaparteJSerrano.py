e51f3294159a288f9315d51e8559fcb3cdf6c997
jaimesema <93590195+jaimesema@users.noreply.github.com>
2022-03-08 12:45:14 +0100

    Update agendaparteJSerrano.py

--- agendaparteJSerrano.py
+++ agendaparteJSerrano.py
@@ -1,5 +1,4 @@
 def buscar_JSerrano(nombre, agenda):
-    cadena = input("¿Qué contacto buscas?: ")
     for nombre, numtel in agenda.items():
         if nombre.startswith(cadena):
             print("El número de teléfono de %s es el %s" % (nombre, agenda [nombre]))
