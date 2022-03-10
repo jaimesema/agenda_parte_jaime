b530432599f78dde48ff6ec5ab4157263bc73945
jaimesema <93590195+jaimesema@users.noreply.github.com>
2022-03-10 09:02:01 +0100

    Update agendaparteJSerrano.py

--- agendaparteJSerrano.py
+++ agendaparteJSerrano.py
@@ -1,4 +1,5 @@
-def buscar_JSerrano(nombre, agenda):
-    for nombre, numtel in agenda.items():
+#Este es el programa de Jaime Serrano Maya y definimos y ponemos en acción la opción buscar dentro de la agenda
+def buscar_JSerrano(nombre, agenda):    #Definimos la operación de buscar y sus variables, nombre y agenda
+    for nombre, numtel in agenda.items(): 
         if nombre.startswith(cadena):
-            print("El número de teléfono de %s es el %s" % (nombre, agenda [nombre]))
+            print("El número de teléfono de %s es el %s" % (nombre, agenda [nombre]))   #Mostramos la frase con el % que es definido en la misma línea como nombre y número
